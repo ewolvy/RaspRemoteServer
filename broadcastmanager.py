ffce8579130f436c1d3c94197a545cb611d421e8
Juanjo Soriano <23655794+ewolvy@users.noreply.github.com>
2019-03-28 10:03:42 +0100

    Initial commit

---
+++ broadcastmanager.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
+# Broadcast manager is intended to use in a Raspberry Pi to send server information for different services
+# controlled by Android applications created by Juanjo Soriano for personal use and distributed
+# as is without any warranty and full freedom for reuse the code, change it, distribute it or do whatever
+# you want, as long as it's kept free (both in money cost and rights for the user).
+
+import socket
+
+# Define the timeout for the sockets. Advice to 10 seconds
+import sys
+
+TIMEOUT = 10
+# Define the UDP and TCP port
+PORT = 19103
+
+# Define RealRemote Data to be sent, JSON formatted
+REAL_REMOTE = ('{"META":{'
+               '"FieldCount":6,'
+               '"Fields":["Name","Brand","Address","Port","Alias","Description"],'
+               '"Description":"Salón en Sa Ràpita"'
+               '},'
+               '"RealRemote":{'
+               '"Name":"Salón",'
+               '"Brand":0,'
+               '"Address":"https://ewolvy.mooo.com",'
+               '"Port":1207,'
+               'Alias":"AAKaysun",'
+               'Description":"Salón en Sa Ràpita"}'
+               '}\n')
+
+# Define Youtube Data to be sent, JSON formatted
+YOUTUBE = ('{"META":'
+           '{"FieldCount":5,'
+           '"Fields":["Name","Address","Port","Alias","Description"],'
+           '"Description":"Salón en Sa Ràpita"'
+           '},'
+           '"YTToPi":{'
+           '"Name":"Salón",'
+           '"Address":"192.168.1.251",'
+           '"Port":21603,'
+           '"Alias":"YTToPi",'
+           '"Description":"Salón en Sa Ràpita"}'
+           '}\n')
+
+# Define CloseKodi Data to be sent, JSON formatted
+CLOSE_KODI = ('{"META":{'
+              '"FieldCount":5,'
+              '"Fields":["Name","Address","Port","Alias","Description"],'
+              '"Description":"Salón en Sa Ràpita"'
+              '},'
+              '"CloseKodi":{'
+              '"Name":"Salón",'
+              '"Address":"https://ewolvy.mooo.com",'
+              '"Port":1207,'
+              '"Alias":"CloseKodi",'
+              '"Description":"Salón en Sa Ràpita"}'
+              '}\n')
+
+# Define RaspRemote Data to be sent, JSON formatted
+RASP_REMOTE = ('{"META":{'
+               '"ObjectCount":2,'
+               '"ObjectNames":["AirConditioner","Heater"]'
+               '},'
+               '"AirConditioner":{'
+               '"Name":"AA Salón",'
+               '"Type":0,'
+               '"Address":"https://ewolvy.mooo.com",'
+               '"Port":1207,'
+               '"Alias":"AAKaysun",'
+               '"Description":"Aire acondicionado en salón de Sa Ràpita"'
+               '},'
+               '"Heater":{'
+               '"Name":"Calefactor",'
+               '"Type":3,"Address":"https://ewolvy.mooo.com",'
+               '"Port":1207,'
+               '"Alias":"HBathroom",'
+               '"Description":"Calefactor en salón de Sa Ràpita"}'
+               '}\n')
+
+
+# Function to create or get the socket:
+# Params:
+#   from_fd: if set to True, will get the socket from File Descriptor handled by Linux Daemon
+#            if set to False, will create a new socket for all interfaces (intended for debugging and testing only)
+def create_udp_socket(from_fd):
+    if from_fd:
+        new_socket = socket.fromfd(3, socket.AF_INET, socket.SOCK_DGRAM)
+    else:
+        new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        address = ('', PORT)
+        new_socket.bind(address)
+    new_socket.settimeout(TIMEOUT)
+    return new_socket
+
+
+# Function to create a TCP socket:
+def create_tcp_socket(address):
+    print address
+    new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    new_socket.connect(address)
+    new_socket.settimeout(TIMEOUT)
+    return new_socket
+
+
+if __name__ == "__main__":
+    isOnLinux = True
+    if sys.argv[1] == '0':
+        isOnLinux = False
+    getSocket = create_udp_socket(isOnLinux)  # True unless specified 0 on command line as first parameter
+    try:
+        data, contactAddress = getSocket.recvfrom(1024)
+        sendSocket = create_tcp_socket((contactAddress[0], 19103))
+        if data == "BROADCAST_REALREMOTE":
+            sent = sendSocket.sendto(REAL_REMOTE, contactAddress)
+            if sent > 0:
+                print "Sent RealRemote data to " + contactAddress[0] + " on port 19103"
+        elif data == "BROADCAST_YTTOPI":
+            sent = sendSocket.sendto(YOUTUBE, contactAddress)
+            if sent > 0:
+                print "Sent Youtube data to " + contactAddress[0] + " on port 19103"
+        elif data == "BROADCAST_CLOSEKODI":
+            sent = sendSocket.sendto(CLOSE_KODI, contactAddress)
+            if sent > 0:
+                print "Sent CloseKodi data to " + contactAddress[0] + " on port 19103"
+        elif data == "BROADCAST_RASPREMOTE":
+            sent = sendSocket.sendto(RASP_REMOTE, contactAddress)
+            if sent > 0:
+                print "Sent RaspRemote data to " + contactAddress[0] + " on port 19103"
+        else:
+            print "No correct data: " + data
+        sendSocket.close()
+    except socket.timeout:
+        print "Timeout after " + TIMEOUT.__str__() + " seconds"
+        exit(1)
+    getSocket.close()
+
---
+++ sslRaspRemote.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import BaseHTTPServer
+import base64
+import json
+import os
+import re
+import ssl
+import sys
+import time
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+key = ""
+init_ir_send = "irsend -d /run/lirc/lircd-lirc0 SEND_ONCE "
+init_rf_send = "python3 /usr/bin/rfsend.py -p 180 -t 1 "
+
+
+class AuthHandler(SimpleHTTPRequestHandler):
+    # Main class to present web pages and authentication.
+
+    def execute(self, command):
+        print(command)
+        result = os.system(command)
+        self.wfile.write(json.dumps({"Command":result}))
+
+    def do_HEAD(self):
+        print "Send header"
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_AUTHHEAD(self):
+        print "Send header"
+        self.send_response(401)
+        self.send_header('WWW-Authenticate', 'Basic realm=\"Test\"')
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        global key
+        ''' Present front page with user authentication. '''
+        if self.headers.getheader('Authorization') is None:
+            self.do_AUTHHEAD()
+            self.wfile.write('No auth header received')
+            pass
+        elif self.headers.getheader('Authorization') == 'Basic ' + key:
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            if re.match('/AAProKlima/', self.path) is not None:
+                send_command = init_ir_send + "AAProKlima " + self.path[12:]
+                self.execute(send_command)
+            elif re.match('/AAKaysun/', self.path) is not None:
+                send_command = init_ir_send + "AAKaysun " + self.path[10:]
+                self.execute(send_command)
+            elif re.match('/HBathroom/', self.path) is not None:
+                send_command = init_rf_send + self.path[11:]
+                self.execute(send_command)
+            elif re.match('/exitprogram/', self.path) is not None:
+                self.wfile.write(json.dumps({"Exit": 0}))
+                time.sleep(1)
+                print "Exiting"
+                sys.exit(0)
+            pass
+        else:
+            self.do_AUTHHEAD()
+            self.wfile.write(self.headers.getheader('Authorization'))
+            self.wfile.write('Not authenticated')
+            pass
+
+
+def create_server(port, password):
+    global key
+    key = base64.b64encode(password)
+    httpd = BaseHTTPServer.HTTPServer(('', port), AuthHandler)
+    httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True,
+                                   # certfile='/home/osmc/ewolvy.mooo.com.pem',
+                                   # keyfile='/home/osmc/ewolvy.mooo.com.private.pem',
+                                   certfile='./ewolvy.mooo.com.pem',
+                                   keyfile='./ewolvy.mooo.com.private.pem',
+                                   ssl_version=ssl.PROTOCOL_TLSv1)
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print "usage sslRaspRemote.py [port] [username:password]"
+        sys.exit()
+    create_server(int(sys.argv[1]), sys.argv[2])
